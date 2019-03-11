6b0f0efdc6c2d424edbef6905a540e3508fcf627
丁丁 <Kutinging@users.noreply.github.com>
2019-03-11 18:50:51 +0800

    Add class and function
    Add function AESCBC.encrypt(key, iv, data) , AESCBC.b(s).
    Add class B64, HTTPClient.
    Modify class UDID's function.

--- ThanatosDi/module.py
+++ ThanatosDi/module.py
@@ -1,11 +1,24 @@
 import base64
 from random import randint
 
-from Crypto.Cipher import AES
+import requests #requests
+from Crypto.Cipher import AES #pycryptodome
 
 
 class AESCBC:
-    """aes class"""
+    """AES decrypt and encrypt function. AES.decrypt(key, iv, data), AES.encrypt(key, iv, data)"""
+
+    def __init__(self):
+        print(
+            """AES decrypt and encrypt function.
+    AESCBC.decrypt(key:bytes, iv:bytes, data:bytes)
+        decrypt data.
+    AESCBC.encrypt(key:bytes, iv:bytes, data:bytes)
+        encrypt data
+    AESCBC.b(s:str)
+        convert s(str) to bytes"""
+    )
+
     @staticmethod
     def decrypt(key, iv, data):
         try:
@@ -13,7 +26,7 @@ class AESCBC:
             new = cryptor.decrypt(data)
             return new
         except Exception as e:
-            return f'decrypt error : {str(e)}'
+            return print(f'AESCBC.decrypt : {str(e)}')
 
     @staticmethod
     def encrypt(key, iv, data):
@@ -22,20 +35,52 @@ class AESCBC:
             new = cryptor.encrypt(data)
             return new
         except Exception as e:
-            return f'encrypt error : {str(e)}'
+            return print(f'AESCBC.encrypt : {str(e)}')
+
+    @staticmethod
+    def b(s: str):
+        try:
+            return str.encode(s)
+        except Exception as e:
+            return print(f'string.b : {str(e)}')
+
+
+class B64:
+    """ base64 encode and decode function. """
+
+    def __init__(self):
+        print("""base64 encode and decode function.\n   B64.encode(s:str)\n   B64.decode(s:str)""")
+
+    @staticmethod
+    def encode(s:str):
+        """ encode string to base64 """
+        try:
+            return base64.b64encode(str(s))
+        except Exception as e:
+            return print(f'B64.encode : {str(e)}')
+
+    @staticmethod
+    def decode(s:str):
+        """ decode string to base64 """
+        try:
+            return base64.b64decode(str(s))
+        except Exception as e:
+            return print(f'B64.decode : {str(e)}')
 
 
 class UDID:
-    """udid class"""
-    def encode(self, s):
+    """UDID encode and decode function. UDID.encode(string), UDID.decode(string)"""
+    @staticmethod
+    def encode(s:str):
         arr = []
         for i in range(len(s)):
             c = s[i]
-            arr.append(self.createRandomNumberString(
-                2) + self.chr(self.ord(c) + 10) + self.createRandomNumberString(1))
-        return self._04x(len(s)) + ''.join(arr) + self.createRandomNumberString(32)
+            arr.append(UDID().createRandomNumberString(
+                2) + UDID().chr(UDID().ord(c) + 10) + UDID().createRandomNumberString(1))
+        return UDID()._04x(len(s)) + ''.join(arr) + UDID().createRandomNumberString(32)
 
-    def decode(self, s):
+    @staticmethod
+    def decode(s:str):
         l = int(str(s[0:4]), 16)
         e = ''
         for i in range(6, len(s), 4):
@@ -43,7 +88,7 @@ class UDID:
         e = e[0:l]
         arr = []
         for i in range(len(e)):
-            arr.append(self.chr(self.ord(e[i]) - 10))
+            arr.append(UDID().chr(UDID().ord(e[i]) - 10))
         return ''.join(arr)
 
     def _04x(self, n: int):
@@ -62,3 +107,13 @@ class UDID:
 
     def chr(self, code):
         return chr(int(code))
+
+
+class HTTPClient:
+    """ HTTPClient class post data to server and return response. """
+
+    def make_request(self, method, endpoint, headers=None, data=None):
+        with requests.request(method, endpoint, headers=headers, data=data) as resp:
+            if resp.status_code == 200:
+                resp = resp.encoding('utf-8')
+                return resp.text
