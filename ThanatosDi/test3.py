6f7bf9a44a77cb391c9d20a1992e3e5fe56209aa
丁丁 <Kutinging@users.noreply.github.com>
2019-03-14 01:33:49 +0800

    test3 測試檔

--- ThanatosDi/test3.py
+++ ThanatosDi/test3.py
@@ -5,15 +5,15 @@ API = 'https://api-pc.so-net.tw'
 headers = {
     'PARAM': '21d7337286f43a47527a9b4a674fb65ace784736',
     'KEYCHAIN': '',
-    'APP_VER': '1.3.0',
-    'RES_VER': '00005000',
+    'APP_VER': '1.3.2',
+    'RES_VER': '00005001',
     'PLATFORM': '2',
     'LOCALE': 'Jpn',
     'IP_ADDRESS': '172.17.100.15',
     'DEVICE_NAME': 'samsung SM-G955N',
     'X-Unity-Version': '2017.1.2p2',
     'Content-Type': 'application/x-www-form-urlencoded',
-    'SID': 'c2240ae8e024efced1bc6d297066c72a',
+    'SID': '0a23416d1066739c598edf69fbe6b779',
     'GRAPHICS_DEVICE_NAME': 'Adreno (TM) 540',
     'DEVICE_ID': '16a470d0c845244e43e4b54c287a2480',
     'REGION_CODE': '',
@@ -30,33 +30,28 @@ client = HTTPClient()
 
 
 if __name__ == "__main__":
-    udid = '88d177ecf20241e39ef2a9c9dd6bc17c'
+    udid = 'ba0175055f4c4929bd917a3344868032'  
+    # ba017505-5f4c-4929-bd91-7a3344868032
+    # 587112363
     #response = client.make_request('POST',f'{API}/check/game_start',headers=headers, verify=True)
-    d = 'kdbR/56gs7SGLylrOmzv3sGRJx63NyjMXJEa4SNBcJjFzuoZLbT6xtSzLFp2B0plontLuVkjPE4/9nB/gcgqaeDyg8tq7ubMwgpzoIu8fIfq8C9jEwrsG0cdTFwfhLuimHV/Q00hoyx6U17WQlv5CBaDgnFApLKjd9KW4stg2KUR04POLlJLdMHfuRTVAhcvO9yE31PyrxcaCLHDzOw+Rt4XN2YM9JdaBE/vyq5PBN/bBkUjS6cKKOO7PP3E1rbEXKICiGD4xEXf2r4Lj9pIa1QyttmqMGNnorJhRNgaqPRsnMINlzi8Q86egMRb9v+3yjtI8PsMtapmb2lXQP+EA7TWYQtasBw5tNOAjEJnnigBlMnFxn57uMib9TZqyaf1llJgSORcDVY0VmFf0w5xcTOIFPi0zVyaiUM20t+xYXKL+4Ol/v4aELd0pkheWpesk7uUsORZHug5BzZ+IDtfhD0qalr+dGWFq1Jj/G5jrLg345hIiEhs743cz6/KlDu4E5hEPLIbBFDVhlXoHsuCD+T+0w/baqgo856JivmDgn5lZjhkZjIwNWZhZjJhNzdmOGFkYmNhNzRhZDQ4NmVmNA=='
-    """ d
-    \x87
-    \xa8app_type\x00
-    \xadcampaign_data\xa0
-    \xadcampaign_sign\xa0
-    \xadcampaign_user\xd2\x00\x02\x8b\x9e
-    \xabendpointArn\xa0
-    \xabcountrycode\xa2CN
-    \xa9viewer_id
-    \xda\x00@DJqDuanIhlBAqklYwh5jOk5EWmpNVE13T0RJNE1HWTFaRE13WVRKak5UWTNaR015
 
-    \x87\xa8app_type\x00\xadcampaign_data\xa0\xadcampaign_sign\xa0\xadcampaign_user\xd2\x00\x02\x8b\x9e\xabendpointArn\xa0\xabcountrycode\xa2CN\xa9viewer_id\xda\x00@DJqDuanIhlBAqklYwh5jOk5EWmpNVE13T0RJNE1HWTFaRE13WVRKak5UWTNaR015
 
-    \x87\xa8app_type\x00\xadcampaign_data\xa0\xadcampaign_sign\xa0\xadcampaign_user\xce\x00\x02\x8b\x9e\xabendpointArn\xa0\xabcountrycode\xa2CN\xa9viewer_id\xda\x00@DJqDuanIhlBAqklYwh5jOk5EWmpNVE13T0RJNE1HWTFaRE13WVRKak5UWTNaR015
-     """
+    d = 'k90eiNA51kC5BHv8wLGCQlpHSmtaakUzWmpkbE0yRTJabUUzWXpZME16a3hNelZq'
     key = Base64(d).key
     data = Base64(d).data
     iv = AESCBC.b(udid[0:16])
 
     resp = AESCBC.decrypt(key, iv, data)
-    #print(resp)
+    print(d)
+    print(resp)
 
-    print(MSP(3).pack)
-    print(str(MSP(b'\xa13').unpack))
-    a = msgpack.unpackb(resp,raw=False)
-    print(a)
-    
+    print('\r\nkey 解密')
+    print(key)
+    k1 = Base64(key.decode('utf-8')).decode
+    print(k1)
+    #a = msgpack.unpackb(resp,raw=False)
+    #print(a)
+    #print(UDID.decode('000924=415A888@111C551@611:856;317?717B481371227114626772317458331621333'))
+    #print(UDID.encode('123456789'))
+    #print(ID.viewerid('698094765'))
+    print(UDID.decode('000975?211B377A842;225;823<524=858@845=528546483723272361472563262742236'))
